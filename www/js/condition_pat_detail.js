23c15bfda43d2817731dec1e865ed83b6094a52f
thirumalacpl <thirumal.acpl@gmail.com>
2016-08-18 19:44:55 +0530

    123eaf

--- www/js/condition_pat_detail.js
+++ www/js/condition_pat_detail.js
@@ -70,6 +70,8 @@ for(a=0;a<patient_detaias_array.length;a++){
 }
 //alert(pat_id_last+'pat_id_last');
 
+alert('asdkjfgksdafjidkaisf');
+alert(navigator.geolocation+'navigator.geolocation');
    navigator.geolocation.getCurrentPosition(onSuccess, onError);  
 
       function onSuccess(position) {
